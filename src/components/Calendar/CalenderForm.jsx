cc03d1bc66adca96a2e59fd9a72ef6e86139c2d3
jeong hana <hj08hn@gmail.com>
2022-12-24 13:22:54 +0900

    create TodoItem componenet
    - 글 제목과 내용을 적는 곳을 컴포넌트화합니다
    
    @Jeong hana

--- src/components/Calendar/CalenderForm.jsx
+++ src/components/Calendar/CalenderForm.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import CommentForm from "../Comments/CommentForm";
 import CommentsList from "../Comments/CommentsList";
 import { Line } from "../UI/Line";
+import TodoItem from "../todo/TodoItem";
 
 const TodoTitleContainer = styled.div`
   margin-top: 5rem;
@@ -13,32 +14,6 @@ const TodoTitleContainer = styled.div`
   text-align: start;
 `;
 
-const TodoTitleStyled = styled.div`
-  max-width: 100%;
-  width: 100%;
-  white-space: pre-wrap;
-  word-break: break-word;
-  caret-color: rgb(55, 53, 47);
-  font-weight: 700;
-  font-size: 40px;
-  padding: 3px 2px;
-  &:focus {
-    outline: none;
-  }
-  [contenteditable]:empty:after,
-  .forcePlaceholder:after {
-    content: attr(placeholder);
-  }
-`;
-
-const TodoDescritionStyled = styled.div`
-  margin-top: 3rem;
-  font-size: 28px;
-  font-weight: 500;
-  &:focus {
-    outline: none;
-  }
-`;
 
 export const CalenderForm = (props) => {
   //  submit 함수
@@ -50,17 +25,7 @@ export const CalenderForm = (props) => {
   return (
     <>
       <TodoTitleContainer>
-        <TodoTitleStyled
-          placeholder="제목 없음"
-          contentEditable={true}
-          spellcheck="true"
-        >
-          제목 없음{" "}
-        </TodoTitleStyled>
-        <Line px="10px" />
-        <TodoDescritionStyled contentEditable={true} spellcheck="true">
-          내용 수정 해봐
-        </TodoDescritionStyled>
+        <TodoItem />
         <Line px="32px" />
         <CommentsList />
         <CommentForm />
