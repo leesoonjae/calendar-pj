99bd8fad9d1cb58be2a38b49700e2b7bbcba564d
jeong hana <hj08hn@gmail.com>
2022-12-24 20:05:58 +0900

    edit import
    - import 한 부분 중 사용하지 않는 것들은 없애고 saveButton, DeleteButton 함수를 삭제합니다
    
    @Jeong hana

--- src/components/Calendar/CalenderForm.jsx
+++ src/components/Calendar/CalenderForm.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { addPost, deletePost } from "../../redux/modules/calendarSlice";
 import CommentForm from "../Comments/CommentForm";
 import CommentsList from "../Comments/CommentsList";
-import { Button } from "../UI/Button";
 import { Line } from "../UI/Line";
 import TodoItem from "../todo/TodoItem";
 
@@ -16,26 +14,7 @@ const TodoTitleContainer = styled.div`
   text-align: start;
 `;
 
-
-const SaveButton = styled.div`
-  display: flex;
-  justify-content: end;
-`;
-
 export const CalenderForm = (props) => {
-  const CONFIRM_MESSAGE = `[확인 메세지]\n\n"${props.title}" 정말로 삭제하시겠습니까?\n삭제를 원하지 않는다면 [취소] 버튼을 눌러주세요.`;
-  //  submit 함수
-  const dispatch = useDispatch();
-  // 확인 메세지
-
-  const handleSaveButton = () => dispatch(addPost(props.id));
-  const handleDeleteButton = () => {
-    if (window.confirm(CONFIRM_MESSAGE)) dispatch(deletePost(props.id));
-  };
-
-  // const submitHandler = (e) => {
-  //   // 유효성 검사
-  // };
   return (
     <>
       <TodoTitleContainer>
@@ -45,8 +24,6 @@ export const CalenderForm = (props) => {
         <CommentForm />
         <Line px="0px" />
       </TodoTitleContainer>
-
-      <div></div>
     </>
   );
 };
